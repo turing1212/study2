3b5d812f103c4c5a0417eba847cd0574542231a0
FWturing <112751691+turing1212@users.noreply.github.com>
2023-04-24 13:48:05 +0800

    Add files via upload
    修改MSE和方差均值计算的过程

--- resultplot.m
+++ resultplot.m
@@ -1,27 +1,28 @@
 %% Draw line graph of MSE
 MSEmat = zeros(max_components,num_repeats);
 for k = 1:K
-    MSEmat = (MSEmat + cellMSEinner{k,1})/10;
+    MSEmat = MSEmat + cellMSEinner{k,1};
 end
+MSEmat = MSEmat / 10;
 MSEvec = mean(MSEmat,2);
-[minMSE,minid] = min(MSEvec);
 plot(1:max_components,MSEvec, 'b-');
 hold on
-plot(minid,minMSE, 'ro');
+plot(opti_ncomp(best_idx),MSEvec(opti_ncomp(best_idx)), 'ro');
 xlabel('Number of components');
 ylabel('MSE');
 title('MSE versus Number of Components');
 %% Draw line graph of the percent of variance explained 
 VARmat = zeros(max_components,num_repeats);
 for k = 1:K
-    VARmat = (VARmat + cellVARinner{k,1})/10;
+    VARmat = VARmat + cellVARinner{k,1};
 end
+VARmat = VARmat / 10;
 VARvec = mean(VARmat,2);
-corVAR = VARvec(minid);
+corVAR = VARvec(opti_ncomp(best_idx));
 figure(2)
 plot(1:max_components,VARvec, 'g-');
 hold on
-plot(minid,corVAR, 'ro');
+plot(opti_ncomp(best_idx),corVAR, 'ro');
 xlabel('Number of components');
 ylabel('The percent of variance explained ');
 title('Percent of variance explained versus Number of Components');
